17a19f405b3bdc4b91d610dc5d087e79939922e8
Ankit Karnam <ankizleyt@gmail.com>
2019-12-17 18:43:18 -0500

    switch to used websockets instead of ajax and it is more efficient

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const http = require('http')
+, https = require('https')
 , express = require('express')
 , bp = require('body-parser')
 , pug = require('pug')
@@ -6,6 +7,7 @@ const http = require('http')
 , app = express();
 
 const server = http.createServer(app).listen(process.env.PORT || 80);
+const io = require('socket.io')(server);
 
 app.use(bp.json());
 app.use(bp.urlencoded({
@@ -15,30 +17,32 @@ app.use(bp.urlencoded({
 app.set('view engine', 'pug');
 app.set('views', './');
 
-app.get('/', (req, res) => {
-  res.render('index');
+io.on('connection', (socket) => {
+  socket.on('spawn', (dataString) => {
+    let data = JSON.parse(dataString);
+
+    var pin = parseFloat(data.pin)
+    , name = data.name
+    , amt = parseInt(data.amt);
+
+    function spawn(i) {
+
+      let client = new Kahoot;
+
+      client.join(pin, name + i);
+
+      client.on('question', question => {
+
+        question.answer(0);
+      });
+
+      setTimeout(() => spawn(i + 1), 50);
+    }
+
+    spawn(1);
+  })
 });
 
-app.post('/break', (req, res) => {
-  var pin = parseFloat(req.body.pin)
-  , name = req.body.name
-  , amt = parseInt(req.body.amt);
-
-  function spawn(i) {
-
-    client = new Kahoot;
-
-    client.join(pin, name + i);
-
-    client.on('question', question => {
-
-      question.answer(0);
-    });
-
-    if (i >= amt) return;
-
-    setTimeout(() => spawn(i + 1), 100);
-  }
-
-  spawn(1);
+app.get('/', (req, res) => {
+  res.render('index');
 });
